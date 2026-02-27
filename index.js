e6e43495fe31a277bcb4a25e653ccc1837853fe8
Kvan2n <102384837+Kvan2n@users.noreply.github.com>
2026-02-27 14:29:08 +0100

    anything can enter, nothing can leave

--- index.js
+++ index.js
@@ -22,33 +22,63 @@ var render = Render.create({
 
 const CATEGORIES = {
     default: 0x0001,
-    wall: 0x0002
+    leftWall: 0x0002,
+    rightWall: 0x0004,
+    groundWall: 0x0008,
+    topWall: 0x0016,
+
+    allWalls: 0x0002 | 0x0004 | 0x0008 | 0x0016,
+    ALL: 0x0001 | 0x0002 | 0x0004 | 0x0008 | 0x0016
 };
 
 
 // create walls
 const offset = 5;
-const wallOptions = {
+
+
+//PLEASE MERGE THESE INTO ONE OR MAKE IT LESS COPYPASTE
+const groundOptions = {
     isStatic: true,
     collisionFilter: {
-        category: CATEGORIES.wall,
-        mask: CATEGORIES.default
+        mask: CATEGORIES.ALL,
+        category: CATEGORIES.groundWall
+    }
+};
+const leftOptions = {
+    isStatic: true,
+    collisionFilter: {
+        mask: CATEGORIES.ALL,
+        category: CATEGORIES.leftWall
+    }
+};
+const rightOptions = {
+    isStatic: true,
+    collisionFilter: {
+        mask: CATEGORIES.ALL,
+        category: CATEGORIES.rightWall
+    }
+};
+
+const topOptions = {
+    isStatic: true,
+    collisionFilter: {
+        mask: CATEGORIES.ALL,
+        category: CATEGORIES.topWall
     }
 };
 
 
-
 var walls = {
     ground: {
-        body: Bodies.rectangle(container.offsetWidth/2, container.offsetHeight+offset, container.offsetWidth, offset*2, wallOptions),
+        body: Bodies.rectangle(container.offsetWidth/2, container.offsetHeight+offset, container.offsetWidth, offset*2, groundOptions),
         normal: { x: 0, y: -1 },
     },
     left: {
-        body: Bodies.rectangle(-offset, container.offsetHeight/2, offset*2, container.offsetHeight, wallOptions),
+        body: Bodies.rectangle(-offset, container.offsetHeight/2, offset*2, container.offsetHeight, leftOptions),
         normal: { x: 1, y: 0 },
     },
     right: {
-        body: Bodies.rectangle(container.offsetWidth+offset, container.offsetHeight/2, offset*2, container.offsetHeight, wallOptions),
+        body: Bodies.rectangle(container.offsetWidth+offset, container.offsetHeight/2, offset*2, container.offsetHeight, rightOptions),
         normal: { x: -1, y: 0 },
     }
 };
@@ -65,7 +95,12 @@ var mouseConstraint = Matter.MouseConstraint.create(engine, {
         render: {
             visible: false
         }
-    }
+    },
+    /*collisionFilter: {
+        category: 0x0000,
+        mask: CATEGORIES.ALL
+    },*/
+    label: "mouse"
 });
 
 Composite.add(engine.world, mouseConstraint);
@@ -78,19 +113,35 @@ Composite.add(engine.world, mouseConstraint);
 // run the renderer
 Render.run(render);
 
-//FIX THIS MESS BY USING ONE CATEGORY PER WALL!!
+
+// disable wall collision for objects moving toward the box
+
 Matter.Events.on(engine, "beforeUpdate", function() {
     Composite.allBodies(engine.world).forEach(body => {
-        if (body.velocity.x < 0) {
-            walls.right.body.collisionFilter.mask = 0x0000;
+        if (body.label == "wallpass") {
+            
+            var mask = CATEGORIES.default
+            if (body.velocity.x > 0) {
+                mask |= CATEGORIES.rightWall
+            }
+            else if (body.velocity.x < 0) {
+                mask |= CATEGORIES.leftWall;
+            }
+            if (body.velocity.y > 0) {
+                mask |= CATEGORIES.groundWall
+            }
+            else if (body.velocity.y < 0) {
+                mask |= CATEGORIES.topWall
+            }
+            
+            body.collisionFilter.mask = mask
+
         }
-        else if (body.velocity.x > 0) {
-            body.collisionFilter.mask = CATEGORIES.default;
-        }
+        
     });
 });
 
-
+//Matter.Events.on(engine, "afterUpdate", function() {console.log(Composite.get(engine.world, 1, "body").collisionFilter)})
 
 
 
@@ -107,7 +158,7 @@ function newElementToContainer(element, x, y, width, height, color) {
     element.style.height = height + 'px';
     element.style.backgroundColor = color;
 
-    var body = Bodies.rectangle(x, y, width, height, { restitution: 0.6, collisionFilter: { category: CATEGORIES.default, mask: CATEGORIES.wall | CATEGORIES.default} });
+    var body = Bodies.rectangle(x, y, width, height, { restitution: 0.6, collisionFilter: { category: CATEGORIES.default, mask: CATEGORIES.ALL} , label: "wallpass"});
     elements[body.id] = element;
     Composite.add(engine.world, body);
 }
